8d424453f0aadc8d0b3b533b2010035ac66c9f51
Tim Tessier <ttessier@swhistlesoft.com>
2025-03-27 11:37:43 -0400

    Version text fix

--- winvde_switch/version.c
+++ winvde_switch/version.c
@@ -1,5 +1,5 @@
 #include "version.h"
 
-char header[] = "VDE switch V.%s\n(C) Virtual Square Team (coord. R. Davoli) 2005,2006,2007 - GPLv2\n";
+char header[] = "WINVDE switch V.%s\n(C) Based on Virtual Square Team (coord. R. Davoli) 2005,2006,2007 - GPLv2\nWinPort started Tim Tessier\n";
 char prompt[] = "\nvde$ ";
 char* EOS = "9999 END OF SESSION";
